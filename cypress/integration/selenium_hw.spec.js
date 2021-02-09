a0ca440c15436b2c1fee020d5a28a5e82ae8a082
Irina1302 <imikhailenko92@gmail.com>
2021-02-10 01:27:35 +0200

    seccond hw

---
+++ cypress/integration/selenium_hw.spec.js
@@ -0,0 +1,14 @@
+describe('MI HW', () => {
+  beforeEach(function () {
+      cy.visit('https://the-internet.herokuapp.com/login');
+  });
+
+it ('Signin with valid creds', () =>{
+  cy.visit('https://the-internet.herokuapp.com/login')
+  cy.get('[name="username"]').type('tomsmith')
+  cy.get('[name="password"]').type('SuperSecretPassword!')
+
+  cy.get('button').click()
+  cy.get('.flash.success').contains('You logged into a secure area!')
+})
+})
