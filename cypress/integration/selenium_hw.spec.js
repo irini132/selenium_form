27917fc78790b72f4bc2039d8692ef6fc0618f50
Irina1302 <imikhailenko92@gmail.com>
2021-02-10 14:45:46 +0200

    second hw

--- cypress/integration/selenium_hw.spec.js
+++ cypress/integration/selenium_hw.spec.js
@@ -2,12 +2,26 @@ describe('MI HW', () => {
   beforeEach(function () {
       cy.visit('https://the-internet.herokuapp.com/login');
   });
+  it ('Signin with valid creds', () =>{
+    // cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('[name="username"]').type('tomsmith')
+    cy.get('[name="password"]').type('SuperSecretPassword!')
 
-it ('Signin with valid creds', () =>{
-  cy.get('[name="username"]').type('tomsmith')
-  cy.get('[name="password"]').type('SuperSecretPassword!')
+    cy.get('button').click()
+    cy.get('.flash.success').contains('You logged into a secure area!')
+  })
+  it ('Signin with invalid creds', () =>{
+    // cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('[name="username"]').type('tomsmith1')
+    cy.get('[name="password"]').type('SuperSecretPassword!1')
 
-  cy.get('button').click()
-  cy.get('.flash.success').contains('You logged into a secure area!')
-})
+    cy.get('button').click()
+    cy.get('.flash.error').contains('Your username is invalid!')
+  })
+    it ('Logout', () =>{
+      cy.get('[name="username"]').type('tomsmith')
+      cy.get('[name="password"]').type('SuperSecretPassword!')
+      cy.get('button').click()
+      cy.get('.button.secondary.radius').click()
+  })
 })
